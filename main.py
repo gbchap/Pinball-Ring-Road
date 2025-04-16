f8c9015fd57ba4f557d54aef975dd3c5e88c7db0
gbchap <gb.freitaschap@gmail.com>
2025-04-16 19:27:55 -0300

    criacaoBase

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    screen.fill((75, 79, 76))
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
