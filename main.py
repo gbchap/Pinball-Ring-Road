d7be1d636b72521e8d141db60e9542bd8be1db7b
gbchap <gb.freitaschap@gmail.com>
2025-07-29 22:42:16 -0300

    trynderror

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ active_piece = None
 
 #pieces class
 class Piece:
-    def __innit__(self, type, image):
+    def __init__(self, type, image, position):
         self.type = type
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (100, 100))
-'''
-pieces = []
-for i in range(3):
-    x = random.randint(978, 1120)
-    y = random.randint(153, 420)
-    Piece("blue", "assets/pieces/blue.png")
-'''
+        self.rect = self.image.get_rect(topleft=position)
+        
+    def move_ip(self, rel):
+        self.rect.move_ip(rel)
+
+
+
 pieces = []
 for i in range(10):
     x = random.randint(978, 1120)
@@ -32,21 +32,25 @@ for i in range(10):
     piece = pygame.Rect(x, y, w, h)
     pieces.append(piece)
 
+
 while running:
     screen.blit(background, (0,0))
     #screen.fill((205,186,244))
     for piece in pieces:    
-        pygame.draw.rect(screen, "red", piece)
+        screen.blit(piece.image, (100, 100))
+
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 for num, piece in enumerate(pieces):
-                    if piece.collidepoint(event.pos):
+                    if piece.rect.collidepoint(event.pos):
                         active_piece = num
+
         if event.type == pygame.MOUSEMOTION:
             #print(event)
             if active_piece != None:
                 pieces[active_piece].move_ip(event.rel)
+
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 active_piece = None
@@ -56,6 +60,7 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
